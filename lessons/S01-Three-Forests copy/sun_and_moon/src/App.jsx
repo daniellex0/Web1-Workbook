f774369938d0b50ac1ed0f3b71bd5ac21e7c7f0b
Danielle N <daniellen@Danielles-MacBook-Pro-2.local>
2020-06-29 18:57:37 -0700

    my first commit

---
+++ lessons/S01-Three-Forests copy/sun_and_moon/src/App.jsx
@@ -0,0 +1,18 @@
+// These are libraries
+import React from 'react';
+
+// import my Custom components
+import SunAndMoon from './SunAndMoon.jsx';
+
+// create a component
+const App = () => {
+    return (
+        <div>
+            App has changed
+            <SunAndMoon />
+        </div>
+    );
+}
+
+// export that component to share
+export default App; 
