19d207bfa8d594857897a27005dc3ae3c32dd8b1
Danielle N <daniellen@Danielles-MBP-2.lan>
2020-07-06 16:57:42 -0700

    add SPA essay

---
+++ Single-Page-App/SPA-in-Class/src/App/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <HeaderStyled>My header</HeaderStyled>
+    );
+}
+
+export default Header;
+
+const HeaderStyled = styled.header`
+    background-color: green;
+    `;
---
+++ Single-Page-App/SPA-in-Class/src/App/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => {
+    return (
+        <NavStyled>My navigation</NavStyled>
+    );
+}
+export default Nav;
+
+const NavStyled = styled.nav`
+    background-color: yellow;
+    `;
