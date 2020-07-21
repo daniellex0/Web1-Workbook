87210cd67fce5d7257f8fb062f15ef7b1f92cd67
Danielle N <daniellen@Danielles-MBP-2.lan>
2020-07-20 17:29:25 -0700

    changes to SPA App and new Essay

--- SPA-in-Class/src/App/Footer.jsx
+++ SPA-in-Class/src/App/Footer.jsx
@@ -4,6 +4,27 @@ import styled from 'styled-components';
 import { mq } from '../common/mediaQueries.js';
 
 const Footer = () => {
+
+    // single line comment
+
+    /*
+        Multi line comment
+        more than one line
+        see
+    */
+
+    const myVariable = 'I am Danielle';
+    const anotherOne = 123;
+    console.log(myVariable, anotherOne);
+
+    const timbuktu = () => {
+        // actions to execute
+        console.log('Executing timbuktu');
+    }
+
+    timbuktu();
+
+
     return (
         <FooterStyled>
             <div className='nested-wrapper'>
