66c1ef5fed9e0e4d79c3de4f32cd53c84d7e22c4
Danielle N <daniellen@Danielles-MBP-2.lan>
2020-08-17 17:15:45 -0700

    Added new essay and updates to SPA in Class App

--- SPA-in-Class/src/App/Pages/Contact/ContactForm.jsx
+++ SPA-in-Class/src/App/Pages/Contact/ContactForm.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+/* Scripts ---------------------------*/
+import API from '../../../common/axios.js';
+
 /* Components ---------------------------*/
 import FormGroup from '../../../common/Forms/FormGroup.jsx';
+import Button from '../../../common/Forms/Button.jsx';
 
 const formFields = {
     nameField: {
@@ -10,18 +14,33 @@ const formFields = {
         label: 'Name',
         inputType: 'text',
         value: '',
+        validate: {
+            required: true,
+            valid: true,
+            message: '',
+        }
     },
     emailField: {
         id: 'userEmail',
         label: 'Email',
         inputType: 'email',
         value: '',
+        validate: {
+            required: true,
+            valid: true,
+            message: '',
+        }
     },
     messageField: {
         id: 'userMessage',
         label: 'Message',
         inputType: 'textarea',
         value: '',
+        validate: {
+            required: false,
+            valid: true,
+            message: '',
+        }
     }
 }
 
@@ -33,11 +52,25 @@ const ContactForm = () => {
 
 
     const handleOnChange = (event, formField) => {
-        console.log(event.target.value, formField);
 
         const newField = {
             ...formField,
             value: event.target.value,
+        }
+
+        if (newField.validate.required === true) {
+            console.log('Validating: ', newField.label);
+        
+
+            if (newField.value.length < 4) {
+                // Let them know there is an error
+                newField.validate.valid = false;
+                newField.validate.message = `You forgot to fill out the ${newField.label} field.`;
+            } else {
+                // Clear my errors
+                newField.validate.valid = true;
+                newField.validate.message = ``;
+            }
         }
 
         if (formField.id === 'userName') {
@@ -51,9 +84,30 @@ const ContactForm = () => {
         }
     }
 
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log('Captured Form Submit, we will take it from here.');
+
+        if ( userName.validate.valid === true && userEmail.validate.valid === true) {
+            console.log('Valid Submitting form.');
+
+            // ...AJAX goes here: AKA AXIOS
+            const postData = {
+                userName: userName,
+                userEmail: userEmail,
+                userMessage: userMessage,
+            }
+
+            API.post('/sendEmail', postData).then((result) => {
+                console.log('result', result);
+            });
+        }
+    
+    }
+
     return (
         <ContactFormStyled className='ContactForm'>
-            <form>
+            <form onSubmit={ handleFormSubmit }>
                 <FormGroup 
                     formField={ userName }
                     onChange={ handleOnChange }
@@ -66,6 +120,8 @@ const ContactForm = () => {
                     formField={ userMessage } 
                     onChange={ handleOnChange }
                 />
+
+                <Button type='submit'>Send Email</Button>
             </form>
         </ContactFormStyled>
     );
--- SPA-in-Class/src/common/Forms/FormGroup.jsx
+++ SPA-in-Class/src/common/Forms/FormGroup.jsx
@@ -11,6 +11,10 @@ const Control = (formField.inputType === 'textarea') ? Textarea : Input;
     return (
         <FormGroupStyled className='FormGroup'>
             <label htmlFor={ formField.id }>{formField.label}:</label>
+            {
+                (formField.validate.message) &&
+                <div className="feedback">{ formField.validate.message }</div>
+            }
             <Control formField={ formField } onChange={ onChange }/>
         </FormGroupStyled>
     );
@@ -19,5 +23,23 @@ const Control = (formField.inputType === 'textarea') ? Textarea : Input;
 export default FormGroup;
 
 const FormGroupStyled = styled.div`
-    
+    label {
+        font-weight: bold;
+    }
+    .feedback {
+        color: red;
+        margin: 5px 0px;
+    }
+
+    .Control {
+        input, textarea {
+            width: 100%;
+            font-size: 18px;
+            padding: 5px;
+        }
+
+        textarea {
+            height: 200px;
+        }
+    }
 `;
